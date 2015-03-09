c9d5950bdf0e62d96c3b33ea40e6c365d5e206c4
Alessio Santo <alessio.santocs@gmail.com>
2015-03-09 13:01:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SaladBuilder::Application.config.secret_key_base = '599c48d51fb58b97185c033d7d086f290f9ebfd7421088eab3471bba16704649feca9d9f15b22da2bd4911bfc497a4570e30e4833e449d05d266136e080950e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SaladBuilder::Application.config.session_store :cookie_store, key: '_salad-builder_session'
